550c29530730501c3d869c205eb7eac5ed76c4e6
Suriya2023 <rajputsuraj221221@gmail.com>
2025-06-30 20:53:43 +0530

    deploye ment error fix

--- frontend/src/Database/axios.js
+++ frontend/src/Database/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5005/api", // 🟢 Update this if port/backend is wrong
-  withCredentials: true, // ✅ Correct way
+  baseURL: "https://chating-app-message.onrender.com/api", // ✅ LIVE backend URL
+  withCredentials: true,
 });
