0994f3e0d0f64121e4a8180822fb18acc1c532dd
Suriya2023 <rajputsuraj221221@gmail.com>
2025-06-30 21:15:59 +0530

    fix debuging

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: "https://chat-list-lake.vercel.app/",
+    origin: "https://chat-list-lake.vercel.app/login",
     credentials: true,
 }));
 
