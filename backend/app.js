ae014c8c791806fb8a36313460b74e0e9e6616fc
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-01 10:26:14 +0530

    change to controller auth and .env toke

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,8 @@ import { app, server } from "./src/DataBase/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-
+app.use(cookieParser());
+app.use(express.json());
 app.use(cors({
     origin: "https://chat-list-lake.vercel.app",
     credentials: true,
@@ -18,7 +19,7 @@ app.use(cors({
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(cookieParser());
+ 
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoute);
--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,7 +4,7 @@ import User from '../models/user.model.js';
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    if (!token) return res.status(401).json({ message: "Unauthorized token" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const user = await User.findById(decoded.userId).select("-password");
