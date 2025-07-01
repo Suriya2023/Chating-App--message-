d5bc14aad0249d7742692c7dd18505b99c925313
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-01 17:36:47 +0530

    change auth middlewarec

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,18 +1,33 @@
 import jwt from 'jsonwebtoken';
 import User from '../models/user.model.js';
 
+
+
+
 export const protectRoute = async (req, res, next) => {
-  try {
-    const token = req.cookies.jwt;
-    if (!token) return res.status(401).json({ message: "Unauthorized token" });
+  const authHeader = req.headers.authorization;
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decoded.userId).select("-password");
-    if (!user) return res.status(401).json({ message: "Unauthorized" });
-
-    req.user = user;
-    next();
-  } catch (err) {
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
+    if (!user) return res.status(401).json({ message: "Forbiden" });
+
+
+
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ message: "Unauthorized", error: err.message });
+  }
 };
+
+
+
