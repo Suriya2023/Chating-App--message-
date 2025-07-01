a52767ce9b7eb886b9031eb18f4da659425610a1
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-01 10:18:20 +0530

    change to controller auth and .env toke

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -4,23 +4,15 @@ import User from '../models/user.model.js';
 export const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
-
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No Token Provided" });
-    }
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     const user = await User.findById(decoded.userId).select("-password");
-    if (!user) {
-      return res.status(404).json({ message: "Unauthorized - User Not Found" });
-    }
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
 
-    req.user = user; 
-    next();  
-
-  } catch (error) {
-    console.error("JWT Auth Error:", error.message);
-    return res.status(401).json({ message: "Unauthorized - Token Invalid or Expired" });
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Unauthorized" });
   }
 };
